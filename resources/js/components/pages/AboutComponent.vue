75b6fe0a4a5bd7c10528b4997cde352f11f72ab3
Mr. CaT <mrcat323@gmail.com>
2019-02-25 20:42:37 +0500

    Components updated

---
+++ resources/js/components/pages/AboutComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <hero :title="title" :sub-title="moreInfo"></hero>
+    </div>
+</template>
+
+<script>
+import Hero from '../layouts/HeroLayout.vue'
+
+export default {
+    data() {
+        return {
+            title: 'About',
+            moreInfo: 'This is about page for you!'
+        }
+    },
+    components: { Hero }
+}
+</script>
+
+<style></style>
---
+++ resources/js/components/pages/HomeComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <hero title="Lara-Shop" sub-title="Free, Open Source E-Commerce"></hero>
+    </div>
+</template>
+
+<script>
+import Hero from '../layouts/HeroLayout.vue'
+
+export default {
+    components: { Hero }
+}
+</script>
+
+<style></style>
