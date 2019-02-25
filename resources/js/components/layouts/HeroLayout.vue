3b39deaa00609d3b9a85ea67f9795fba5a85d73b
Mr. CaT <mrcat323@gmail.com>
2019-02-25 20:34:09 +0500

    Necessary layouts added

---
+++ resources/js/components/layouts/HeroLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <section class="hero is-primary is-large is-bold">
+            <div class="hero-head">
+                <navbar></navbar>
+            </div>
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        {{ title }}
+                    </h1>
+                    <h2 class="subtitle">
+                        {{ subTitle }}
+                    </h2>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from './NavbarLayout.vue'
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: ''
+        },
+        subTitle: {
+            type: String,
+            default: ''
+        }
+    },
+
+    components: { Navbar }
+}
+</script>
+
+<style></style>
---
+++ resources/js/components/layouts/NavbarLayout.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="container">
+                <div class="navbar-brand">
+                    <router-link to="/" class="navbar-item">
+                        Lara-Shop
+                    </router-link>
+
+                    <router-link
+                        role="button"
+                        class="navbar-burger burger"
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbarBasicExample"
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </router-link>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        <router-link
+                            :to="element.path"
+                            class="navbar-item"
+                            v-for="element in elements"
+                            :key="element.id"
+                        >
+                            {{ element.name }}
+                        </router-link>
+                    </div>
+
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <router-link
+                                    to="/register"
+                                    class="button is-primary"
+                                >
+                                    <strong>Sign up</strong>
+                                </router-link>
+                                <router-link
+                                    to="/login"
+                                    class="button is-light"
+                                >
+                                    Log in
+                                </router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            elements: [
+                {
+                    name: 'Home',
+                    path: '/'
+                },
+                {
+                    name: 'About',
+                    path: '/about'
+                },
+                {
+                    name: 'Categories',
+                    path: '/categories/index'
+                },
+                {
+                    name: 'Products',
+                    path: '/products/index'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style></style>
