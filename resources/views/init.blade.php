47e767aa456b06c7240516e124d3ee25d98b6325
Mr. CaT <mrcat323@gmail.com>
2019-02-25 20:40:35 +0500

    Configuring...

--- resources/views/init.blade.php
+++ resources/views/init.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <title>Lara-Bue - Quick Starter - Boost your development</title>
+    <title>Lara-Shop</title>
     
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
