1bab01c1d8d41dc7576db6ecfcde6c1c101f41f4
Mr. CaT <mrcat323@gmail.com>
2019-02-25 20:32:24 +0500

    Configured the repository for products

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            App\Repositories\ProductInterface::class,
+            App\Repositories\ProductRepository::class
+        );
     }
 
     /**
