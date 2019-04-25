084386c4da4e226bdcdd22591d8304100d8710d4
Mr. CaT <mrcat323@gmail.com>
2019-04-25 22:06:21 +0500

    made components loading dynamic

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,14 +4,15 @@ import VueRouter from 'vue-router'
 // it were plugins and vuejs itself
 Vue.use(VueRouter)
 
-import Home from '../components/pages/HomeComponent.vue'
-import About from '../components/pages/AboutComponent.vue'
-
+let Home = () => import(/* webpackChunkName: "home-page" */ '../components/pages/HomeComponent.vue')
+let About = () => import(/* webpackChunkName: "about-page" */ '../components/pages/AboutComponent.vue')
+let Register = () => import(/* webpackChunkName: "register-page" */ '../components/pages/RegisterComponent.vue')
 const router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/', component: Home },
-        { path: '/about', component: About }
+        { path: '/about', component: About },
+	{ path: '/register', component: Register } 
     ]
 })
 
