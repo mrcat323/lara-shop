9fa50c8b18dc96cd27b273b5acecb84015ba9031
Mr. CaT <mrcat323@gmail.com>
2019-05-14 13:45:38 +0500

    registration process

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,8 +44,13 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        return parent::render($request, $exception);
+        if ($e instanceof Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } else if ($e instanceof Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        }
+        return parent::render($request, $e);
     }
 }
--- resources/js/components/pages/RegisterComponent.vue
+++ resources/js/components/pages/RegisterComponent.vue
@@ -1,27 +1,71 @@
 <template>
-    <div class="container">
-      <b-field horizontal label="Name">
-	<b-input placeholder="Your Name" name="name" expanded />
-      </b-field>
-      <b-field horizontal label="E-Mail">
-	<b-input placeholder="Your E-Mail" name="email" type="email" expanded />
-      </b-field>
-      <b-field horizontal label="Password">
-	<b-input placeholder="Your password" name="password" type="password" expanded />
-      </b-field>
-      <b-field horizontal label="Password confirm">
-	<b-input placeholder="Confirm your password" name="password-confirm" type="password" expanded />
-      </b-field>
-      <b-field horizontal>
-	<p class="control">
-	  <button class="button is-primary">Register</button>
-	</p>
-      </b-field>
+  <div>
+    <div class="hero-register">
+      <hero title="Register" sub-title="Sign up to create an account" />
     </div>
+<div class="container">
+  <form @submit.prevent="register" method="post">
+    <b-field horizontal label="Name">
+      <b-input placeholder="Your Name" v-model="name" name="name" expanded />
+    </b-field>
+    <b-field horizontal label="E-Mail">
+      <b-input placeholder="Your E-Mail" v-model="email" name="email" type="email" expanded />
+    </b-field>
+    <b-field horizontal label="Password">
+      <b-input placeholder="Your password" v-model="password" name="password" type="password" expanded />
+    </b-field>
+    <b-field horizontal label="Password confirm">
+      <b-input placeholder="Confirm your password" v-model="password_confirm" name="password-confirm" type="password" expanded />
+    </b-field>
+    <b-field horizontal>
+      <p class="control">
+	<button class="button is-primary" type="submit">Register</button>
+      </p>
+    </b-field>
+  </form>
+</div>
+</div>
 </template>
 
 <script>
-export default {}
+import Hero from '../layouts/HeroLayout.vue'
+export default {
+    components: { Hero },
+    data() {
+	return {
+	    name: '',
+	    email: '',
+	    password: '',
+	    password_confirm: '',
+	    success: false,
+	    error: false,
+	    errormsg: '',
+	    errors: {}
+	}
+    },
+    methods: {
+	register() {
+	    let self = this
+	    this.$auth.register({
+		data: {
+		    name: self.name,
+		    email: self.email,
+		    password: self.password,
+		    password_confirmation: self.password_confirm
+		},
+		success() {
+		    self.success = true
+		    this.$router.push({ name: 'login', params: {successRegistrationRedirect: true}})
+		},
+		error(res) {
+		    self.error = true
+		    self.errormsg = res.response.data.error
+		    self.errors = res.response.data.errors || {}
+		}
+	    });
+	}
+    }
+}
 </script>
 
 <style></style>
