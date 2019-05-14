5b08c243e7bf03f60562c576c3f512f98d425ce8
Mr. CaT <mrcat323@gmail.com>
2019-05-14 13:55:14 +0500

    Authentication stuff. Methods in Controller, plugins gotta ready for playin' with.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+use Illuminate\Http\Request;
+
+use Hash;
+
+use Illuminate\Support\Facades\Auth;
+
+use JWTAuth;
+
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request, User $user)
+    {
+        $v = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:3|confirmed'
+        ]);
+
+        if ($v->fails()) {
+            return response()->json(['status' => 'error'], 401);
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        return response()->json(['status' => 'success'], 200);
+                             
+    }
+
+    public function login(Request $request)
+    {
+        $credentails = $request->only('email', 'password');
+
+        if ($token = $this->guard()->attempt($credentails)) {
+            return response()->json(['status' => 'success'], 200)->header('Authorization', $token);
+        }
+        return response()->json(['error' => 'login_error'], 401);
+    }
+
+    public function logout()
+    {
+        $this->guard()->logout();
+
+        return response()->json(['status' => 'success', 'msg' => 'Logout successfully'], 200);
+    }
+
+    public function refresh()
+    {
+        if ($token = JWTAuth::getToken()) {
+            return response()->json(['status' => 'success'], 200)->header('Authorization', $token);
+        }
+        
+        return response()->json(['error' => 'refresh_token_error'], 401);
+    }
+
+    public function user(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+
+        return response()->json(['status' => 'success', 'data' => $user], 200);
+    }
+
+    private function guard()
+    {
+        return Auth::guard();
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,16 @@ import Vue from 'vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
+import axios from 'axios'
+import auth from './auth'
+import VueAuth from '@websanova/vue-auth'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
+axios.defaults.baseURL = `http://127.0.0.1:8000/api`
+Vue.router = router
+Vue.use(VueAuth, auth)
+Vue.use(router)
 Vue.use(Buefy)
 
 // now the components turn
