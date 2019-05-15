69857469291c16253bdd14fe009591e772c9ebed
Mr. CaT <mrcat323@gmail.com>
2019-05-15 23:37:11 +0500

    Icon font arrived. Material Icons.

--- resources/views/init.blade.php
+++ resources/views/init.blade.php
@@ -14,6 +14,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css" type="text/css" media="screen" />
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
