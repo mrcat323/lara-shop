7ab492f00dc25c865829615ef6e5a16b3433ceea
Mr. CaT <mrcat323@gmail.com>
2019-07-24 21:25:23 +0500

    Delete component for CRUD

---
+++ resources/js/components/DeleteComponent.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		 <button class="button is-danger" @click="emit">
+            <b-icon icon="delete" size="is-medium"></b-icon>
+        </button>
+	</div>
+</template>
+<script>
+export default {
+	props: ['id'],
+	methods: {
+		emit() {
+			this.$emit('delete-by-id', this.id)
+		}
+	}
+}
+</script>
+<style></style>
