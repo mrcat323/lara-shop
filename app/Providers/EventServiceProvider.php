fbe07e4826c2fa4eb613def45a38e03426a91f4d
Mr. CaT <mrcat323@gmail.com>
2019-09-27 11:44:16 -0400

    Registering events with listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ResetPassword;
+use App\Listeners\SentEmailToResetPassword;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        ResetPassword::class => [
+            SentEmailToResetPassword::class
+        ]
     ];
 
     /**
