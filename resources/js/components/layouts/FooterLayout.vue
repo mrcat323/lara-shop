762b1e08af93d1a43f9b05de599939d6b8001923
Mr. CaT <mrcat323@gmail.com>
2019-09-30 01:55:14 -0400

    Footer arrived!

---
+++ resources/js/components/layouts/FooterLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Lara-Shop</strong>
+                Made with <b-icon icon="heart"></b-icon> by Mr. CaT
+            </p>
+            <p>
+                &copy; Copyright 2018-19
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.footer {
+    margin-top: 45px;
+}
+</style>
